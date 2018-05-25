da12f005ee088f174ea42866a1ba1766d2ecb56a
Hugo Prado <hugo4566@gmail.com>
2018-05-25 09:51:08 -0300

    initial setup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','Simple Test','https://media.npr.org/assets/img/2016/06/29/coolandspicycucpage_custom-76cd10cf7f561a3c8bd6330f119995c73213065d-s900-c85.jpg')
+  ];
 
   constructor() { }
 
